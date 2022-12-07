9ced0e2e67aab9edbc68b1cab401a54dda7268eb
LHelge <249865+LHelge@users.noreply.github.com>
2022-12-07 09:48:50 +0100

    Day6: Solved Part 1 and Part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,14 +1,37 @@
 use std::fs;
 
+fn contains_doubles(signal: &str) -> bool {
+    let mut s = String::from(signal);
+    
+    while s.len() > 0 {
+        let c = s.pop().unwrap();
+
+        if s.contains(c) {
+            return true;
+        }
+    }
+
+    // No double found
+    false
+}
+
+fn find_marker(signal: &str, len: usize) -> Option<usize> {
+    for n in len..signal.len() {
+        if !contains_doubles(&signal[n-len..n]) {
+            return Some(n);
+        }
+    }
+    None
+}
+
 fn main() {
     // Read file
-    //let filename = "input.txt";
-    let filename = "input_example.txt";
+    let filename = "input.txt";
     let input = match fs::read_to_string(filename) {
         Ok(f) => f,
         Err(e) => panic!("Problem reading file {}: {}", filename, e)
     };
 
-    // Do stuff
-    println!("{}", input);
+    println!("Result1: {}", find_marker(&input, 4).unwrap_or(0));
+    println!("Result2: {}", find_marker(&input, 14).unwrap_or(0));
 }
