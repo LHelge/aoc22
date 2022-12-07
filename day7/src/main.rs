2186b44627faea6b0774d5bc9a1a78500bc199f6
LHelge <249865+LHelge@users.noreply.github.com>
2022-12-07 22:46:44 +0100

    Day7: Fooling around with data structures for storing a file system tree

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,6 +1,86 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[derive(Debug)]
+struct Command {
+    command: String,
+    arguments: Vec<String>,
+    outputs: Vec<String>
+}
+
+impl Command {
+    fn new(input: &Vec<String>) -> Self {
+        let parts: Vec<&str> = input[0].split(' ').collect();
+
+        let mut cmd = Self { 
+            command: String::from(parts[1]),
+            arguments: Vec::new(), 
+            outputs: Vec::new() 
+        };
+
+        for n in 2..parts.len() {
+            cmd.arguments.push(String::from(parts[n]));
+        }
+        for n in 1..input.len() {
+            cmd.outputs.push(input[n].clone());
+        }
+
+        cmd
+    }
+}
+
+struct DirectoryObject {
+    name: String,
+    content: Vec<FileSystemObject>,
+    parent: Option<&DirectoryObject>
+}
+
+impl DirectoryObject {
+    fn new(name: String) -> Self {
+        Self {
+            name: name,
+            content: Vec::new(),
+            parent: None
+        }
+    }
+}
+
+struct FileObject {
+    size: usize,
+    name: String
+}
+
+enum FileSystemObject {
+    Directory(DirectoryObject),
+    File(FileObject)
+}
+
+struct FileSystem {
+    root: DirectoryObject,
+    cwd: &DirectoryObject
+}
+
+impl FileSystem {
+    fn new() -> Self {
+        let mut root = DirectoryObject::new(String::from("/"));
+
+        Self { 
+            root: root, 
+            cwd: &root }
+    }
+}
+
+fn parse_command(cmd: &Command, filesystem: &mut FileSystem) -> &mut FileSystem {
+
+    match cmd.command.as_str() {
+        "cd" => println!("Change directory to {}", cmd.arguments[0]),
+        "ls" => println!("Listed directory contents {}", cmd.outputs),
+        _ => panic!("Unsupported command: {}", cmd.command)
+    }
+
+    filesystem
+}
+
 fn main() {
     // Read file
     //let filename = "input.txt";
@@ -12,15 +92,32 @@ fn main() {
     let reader = BufReader::new(file);
 
     // Do stuff
+    let mut filesystem = FileSystem::new();
+
     let mut result1 = 0;
     let mut result2 = 0;
+
+    let mut command: Vec<String> = Vec::new(); 
+
     for (i, line) in reader.lines().enumerate() {
         let line = match line {
             Ok(l) => l,
             Err(e) => panic!("Bad input on line {}: {}", i, e)
         };
 
-        println!("{}", line);
+        let parts: Vec<&str> = line.split(' ').collect();
+
+        // New ocmmand
+        if parts[0] == "$" && parts.len() >= 2 {
+            // Parse previous command if exists
+            if command.len() > 0 {
+                let cmd = Command::new(&command);
+                command.clear();
+
+                filesystem = parse_command(&cmd, &mut filesystem)
+            }
+        }
+        command.push(line.clone());
 
         // Solve Part 1
         result1 += 1;
