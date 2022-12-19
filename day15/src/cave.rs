c37bed6a1e150ecfa13143461756ee4b4838cf60
LHelge <249865+LHelge@users.noreply.github.com>
2022-12-19 09:53:25 +0100

    Day15: Solved part 2

--- day15/src/cave.rs
+++ day15/src/cave.rs
@@ -1,8 +1,8 @@
-use std::{collections::{BTreeMap, BTreeSet, HashSet}, fmt::write};
+use std::{collections::{HashMap, HashSet}};
 
 
 
-pub type Coord = (i32, i32);
+pub type Coord = (i64, i64);
 
 #[derive(Debug)]
 pub struct Sensor {
@@ -12,14 +12,14 @@ pub struct Sensor {
 
 #[derive(Debug)]
 pub struct Cave {
-    sensors: BTreeMap<Coord, u32>,
-    beacons: BTreeSet<Coord>,
+    sensors: HashMap<Coord, u64>,
+    beacons: HashSet<Coord>,
     top_left: Coord,
     bottom_right: Coord
 }
 
 
-fn manhattan_distance(one: &Coord, other: &Coord) -> u32 {
+fn manhattan_distance(one: &Coord, other: &Coord) -> u64 {
     one.0.abs_diff(other.0) + 
     one.1.abs_diff(other.1)
 }
@@ -27,10 +27,10 @@ fn manhattan_distance(one: &Coord, other: &Coord) -> u32 {
 impl Cave {
     pub fn new(sensors: &Vec<Sensor>) -> Self {
         let mut cave = Self {
-            sensors: BTreeMap::new(),
-            beacons: BTreeSet::new(),
-            top_left: (i32::MAX, i32::MAX),
-            bottom_right: (i32::MIN, i32::MIN)
+            sensors: HashMap::new(),
+            beacons: HashSet::new(),
+            top_left: (i64::MAX, i64::MAX),
+            bottom_right: (i64::MIN, i64::MIN)
         };
 
         for sensor in sensors {
@@ -44,19 +44,17 @@ impl Cave {
             cave.beacons.insert(sensor.beacon);
         }
 
-        println!("Created cave from {:?} to {:?}", cave.top_left, cave.bottom_right);
-
         cave
     }
 
-    pub fn no_beacon_position_row(&self, y: i32) -> u32 {
-        let mut no_beacon: HashSet<i32> = HashSet::new();
+    pub fn no_beacon_position_row(&self, y: i64) -> usize {
+        let mut no_beacon: HashSet<i64> = HashSet::new();
 
         for (k, v) in self.sensors.iter() {
             let y_distance = y.abs_diff(k.1);
 
             if y_distance <= *v {
-                for x_offset in 0..=v.abs_diff(y_distance) as i32 {
+                for x_offset in 0..=v.abs_diff(y_distance) as i64 {
                     let x_left = k.0 - x_offset;
                     
                     if !self.beacons.contains(&(x_left,y)) {
@@ -71,7 +69,53 @@ impl Cave {
             }
         }
 
-        no_beacon.len() as u32
+        no_beacon.len()
+    }
+
+    pub fn find_free_spot(&self, top_left: &Coord, bottom_right: &Coord) -> Option<Coord> {
+        let stop_x = self.bottom_right.0.min(bottom_right.0);
+        let stop_y = self.bottom_right.1.min(bottom_right.1);
+
+        //println!("stop_x={}, stop_y={}", stop_x, stop_y);
+        let mut pos = top_left.clone();
+
+        loop {
+            let mut found = false;
+            //println!("Checking {:?}", pos);
+
+            // Find sensors within range
+            for (k, v) in self.sensors.iter() {
+                let x_distance = pos.0.abs_diff(k.0);
+                let y_distance = pos.1.abs_diff(k.1);
+    
+                // Check if within range
+                //println!("  Checking beacon at {:?}. ({}+{}) < {}", k, x_distance, y_distance, v);
+                if (x_distance + y_distance) <= *v  {
+                    pos.0 = k.0 + (*v as i64 - y_distance as i64) + 1;
+                    //println!("    Found becon at {:?}={}, advancing to {:?}", k, v, pos);
+                    
+                    if pos.0 > stop_x {
+                        // Go to next line
+                        pos.1 += 1;
+                        pos.0 = top_left.0;
+
+                        // Reached end No spot found
+                        if pos.1 > stop_y {
+                            return None;
+                        }
+                    }
+
+                    found = true;
+                    break;
+                }
+
+            }
+            
+            if found {
+                continue;
+            }
+            return Some(pos);
+        }
     }
 
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,14 +1,12 @@
 use std::{fs};
 use nom::{
     IResult,
-    sequence::separated_pair,
     multi::separated_list1,
     character::complete::{
         newline,
-        u32,
-        i32
+        i64
     },
-    bytes::complete::tag, Parser
+    bytes::complete::tag
 };
 
 pub mod cave;
@@ -16,10 +14,10 @@ use crate::cave::*;
 
 fn coord(s: &str) -> IResult<&str, Coord> {
     let (s, _) = tag("x=")(s)?;
-    let (s, x) = i32(s)?;
+    let (s, x) = i64(s)?;
     let (s, _) = tag(", ")(s)?;
     let (s, _) = tag("y=")(s)?;
-    let (s, y) = i32(s)?;
+    let (s, y) = i64(s)?;
 
     Ok((s, (x,y)))
 }
@@ -40,7 +38,7 @@ fn sensors(s: &str) -> IResult<&str, Vec<Sensor>> {
     )(s)
 }
 
-fn part1(input: &str, row: i32) -> u32 {
+fn part1(input: &str, row: i64) -> usize {
     // Parse input
     let (_, sensors) = sensors(input).expect("Bad input");
     let cave = Cave::new(&sensors);
@@ -49,6 +47,15 @@ fn part1(input: &str, row: i32) -> u32 {
     cave.no_beacon_position_row(row)
 }
 
+
+fn part2(input: &str, top_left: Coord, bottom_right: Coord) -> u64 {
+    let (_, sensors) = sensors(input).expect("Bad input");
+    let cave = Cave::new(&sensors);
+
+    let beacon = cave.find_free_spot(&top_left, &bottom_right).unwrap();
+
+    (beacon.0 * 4_000_000 + beacon.1) as u64
+}
 
 
 fn main() {
@@ -60,7 +67,7 @@ fn main() {
     println!("Example={}", part1(input_ex.as_str(), 10));
     println!("main={}", part1(input.as_str(), 2000000));
 
-    //println!("Part2:");
-    //println!("Example={}", part2(input_ex.as_str()));
-    //println!("main={}", part2(input.as_str()));
+    println!("Part2:");
+    println!("Example={}", part2(input_ex.as_str(), (0,0), (20, 20)));
+    println!("main={}", part2(input.as_str(), (0,0), (4_000_000, 4_000_000)));
 }
