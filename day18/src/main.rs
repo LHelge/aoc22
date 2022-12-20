f439e7d3fffa11961ffebcc2f084c33436be524d
LHelge <249865+LHelge@users.noreply.github.com>
2022-12-20 15:28:34 +0100

    Day18: Worked on part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, ops::RangeInclusive};
 
 use nom::{
     IResult,
@@ -11,6 +11,56 @@ use nom::{
 };
 
 type Position = (i64, i64, i64);
+
+// Define which offsets to look in for adjacent cubes
+const OFFSETS: [Position; 6] = [
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, -1, 0),
+    (0, 1, 0),
+    (0, 0, -1),
+    (0, 0, 1)
+];
+
+
+#[derive(Debug)]
+struct Bounds {
+    min: Position,
+    max: Position
+}
+
+
+impl Bounds {
+    fn from(cubes: &Vec<Position>) -> Self {
+        let mut bounds = Self { 
+            min: (i64::MAX, i64::MAX, i64::MAX), 
+            max: (i64::MIN, i64::MIN, i64::MIN)
+        };
+
+        for cube in cubes {
+            bounds.min.0 = bounds.min.0.min(cube.0);
+            bounds.min.1 = bounds.min.1.min(cube.1);
+            bounds.min.2 = bounds.min.2.min(cube.2);
+            bounds.max.0 = bounds.max.0.max(cube.0);
+            bounds.max.1 = bounds.max.1.max(cube.1);
+            bounds.max.2 = bounds.max.2.max(cube.2);
+        }
+
+        bounds
+    }
+
+    fn x_range(&self) -> RangeInclusive<i64> {
+        self.min.0..=self.max.0
+    }
+
+    fn y_range(&self) -> RangeInclusive<i64> {
+        self.min.1..=self.max.1
+    }
+
+    fn z_range(&self) -> RangeInclusive<i64> {
+        self.min.2..=self.max.2
+    }
+}
 
 // Parse cubes from text using nom
 fn cubes(s: &str) -> IResult<&str, Vec<Position>> {
@@ -28,16 +78,6 @@ fn part1(input: &str) -> usize {
     // Parse cubes into HashSet<Position>
     let cubes: HashSet<Position> = cubes(input).unwrap().1.into_iter().collect();
 
-    // Define which offsets to look in for adjacent cubes
-    const OFFSETS: [Position; 6] = [
-        (-1, 0, 0),
-        (1, 0, 0),
-        (0, -1, 0),
-        (0, 1, 0),
-        (0, 0, -1),
-        (0, 0, 1)
-    ];
-
     // Calculate surface area
     cubes
         .iter()
@@ -53,6 +93,60 @@ fn part1(input: &str) -> usize {
 }
 
 
+fn part2(input: &str) -> usize {
+    let cubes = cubes(input).unwrap().1;
+    let bounds = Bounds::from(&cubes);
+    let lava: HashSet<Position> = cubes.into_iter().collect();
+
+    // Analyze slices to find "outside"
+    let mut outside: HashSet<Position> = HashSet::new();
+    for x in bounds.x_range() {
+        for y in bounds.y_range() {
+            for z in bounds.z_range() {
+                // TODO: Find all outside positions
+                if !lava.contains(&(x,y,z)) {
+                    outside.insert((x,y,z));
+                }
+            }
+        }
+    }
+    // For now, remove the known void from the example.
+    outside.remove(&(2,2,5));
+    
+    let mut inside_faces = 0usize;
+    for x in bounds.x_range() {
+        for y in bounds.y_range() {
+            for z in bounds.z_range() {
+                if !outside.contains(&(x,y,z)) && !lava.contains(&(x,y,z)) {
+                    // Inside bubble
+                    inside_faces += OFFSETS
+                    .iter()
+                    .filter(|(ox, oy, oz)| {
+                        lava.contains(&(x-ox, y-oy, z-oz))
+                    })
+                    .count();
+
+                    println!("Inside pos {:?}, total {} faces", (x,y,z), inside_faces);
+                }
+            }
+        }
+    }
+
+    let lava_faces = lava
+        .iter()
+        .map(|(x,y,z)| {
+            OFFSETS
+                .iter()
+                .filter(|(ox, oy, oz)| {
+                    !lava.contains(&(x-ox, y-oy, z-oz))
+                })
+                .count()
+        })
+        .sum::<usize>();
+
+    lava_faces - inside_faces
+}
+
 fn main() {
     let input_ex = include_str!("../input_example.txt");
     let input = include_str!("../input.txt");
@@ -60,4 +154,8 @@ fn main() {
     println!("Part1");
     println!("Example={}", part1(input_ex));
     println!("Main={}", part1(input));
+
+    println!("Part2");
+    println!("Example={}", part2(input_ex));
+    //println!("Main={}", part2(input));
 }
